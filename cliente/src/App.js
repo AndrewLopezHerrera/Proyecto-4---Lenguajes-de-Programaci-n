87b1d78e61f638363af266014ea2b5c53e215eb5
Mynell2022 <jmyerschaves@gmail.com>
2024-06-11 19:58:42 -0600

    arreglos

--- cliente/src/App.js
+++ cliente/src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import './App.css';
 import './script.js';
-import './index.css';
 
 function App() {
   return (
-    <div className="App">
+    <div class="App">
     </div>
   );
 }
